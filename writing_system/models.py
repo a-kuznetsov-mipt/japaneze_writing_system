7e367e63d6016f51f99172385a847588989b490e
Alexander Kuznetsov <166259762+a-kuznetsov-mipt@users.noreply.github.com>
2024-05-19 17:39:17 +0300

    writing_system/models.py: the class BaseModel added
    The class is abstract in terms of Django models and contains time_create and time_update fields, which purposes are obvious.

--- writing_system/models.py
+++ writing_system/models.py
@@ -1,3 +1,12 @@
+from datetime import datetime
+
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    """Абстрактная модель, содержащая поля времени создания и изменения."""
+    class Meta:
+        abstract = True
+
+    time_create: datetime = models.DateTimeField(verbose_name='Время создания', auto_now_add=True)
+    time_update: datetime = models.DateTimeField(verbose_name='Время изменения', auto_now=True)
