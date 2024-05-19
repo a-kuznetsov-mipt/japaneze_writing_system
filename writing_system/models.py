c5412a0a08d73265bae12f3c1772aae5ffad353a
Alexander Kuznetsov <166259762+a-kuznetsov-mipt@users.noreply.github.com>
2024-05-19 17:43:34 +0300

    writing_system/models.py: the class HiraganaLetter added

--- writing_system/models.py
+++ writing_system/models.py
@@ -10,3 +10,13 @@ class BaseModel(models.Model):
 
     time_create: datetime = models.DateTimeField(verbose_name='Время создания', auto_now_add=True)
     time_update: datetime = models.DateTimeField(verbose_name='Время изменения', auto_now=True)
+
+
+class HiraganaLetter(BaseModel):
+    class Meta:
+        verbose_name = 'Буква хираганы'
+        verbose_name_plural = 'Буквы хираганы'
+
+    id: int
+    hepburn_romanization = models.CharField('Запись в системе Хепбёрна', max_length=256)
+    image = models.ImageField('Изображение буквы', upload_to='hiragana_letters')
