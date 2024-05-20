8e6a53f47cf8887e6c48b8c2c47e05fff620bc6b
Alexander Kuznetsov <166259762+a-kuznetsov-mipt@users.noreply.github.com>
2024-05-20 17:18:40 +0300

    writing_system/views.py: templates of view classes added
    This are all the views which are planned to be developed in this project yet.

--- writing_system/views.py
+++ writing_system/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+
 
 # Create your views here.
+
+class HomeView(TemplateView):
+    """Класс представления начальной страницы сайта."""
+    template_name = 'writing_system/base.html'
+
+
+class HiraganaListView(TemplateView):
+    """Класс представления списка букв хираганы."""
+    template_name = 'writing_system/base.html'
+
+
+class KatakanaListView(TemplateView):
+    """Класс представления списка букв катаканы."""
+    template_name = 'writing_system/base.html'
+
+
+class KanjiListView(TemplateView):
+    """Класс представления списка букв кандзи."""
+    template_name = 'writing_system/base.html'
+
+
+class HiraganaFormView(TemplateView):
+    """Класс представления страницы создания буквы хираганы."""
+    template_name = 'writing_system/base.html'
+
+
+class KatakanaFormView(TemplateView):
+    """Класс представления страницы создания буквы хираганы."""
+    template_name = 'writing_system/base.html'
+
+
+class KanjiFormView(TemplateView):
+    """Класс представления страницы создания буквы хираганы."""
+    template_name = 'writing_system/base.html'
