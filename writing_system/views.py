988208d139b2f34a4785022b6afdb9e983158229
Alexander Kuznetsov <166259762+a-kuznetsov-mipt@users.noreply.github.com>
2024-05-20 22:12:46 +0300

    writing_system/views.py: views dedicated to showing lists of letters became fully functional
    These classes are quite similar so it's ok to add them in a single commit

--- writing_system/views.py
+++ writing_system/views.py
@@ -1,4 +1,7 @@
-from django.views.generic import TemplateView
+from django.db.models import QuerySet
+from django.views.generic import TemplateView, ListView
+
+from writing_system import models
 
 
 # Create your views here.
@@ -8,19 +11,32 @@ class HomeView(TemplateView):
     template_name = 'writing_system/home.html'
 
 
-class HiraganaListView(TemplateView):
+class HiraganaListView(ListView):
     """Класс представления списка букв хираганы."""
-    template_name = 'writing_system/base.html'
+    template_name = 'writing_system/list_hiragana.html'
+    context_object_name = 'hiragana_letters'
+
+    def get_queryset(self) -> QuerySet[models.HiraganaLetter]:
+        return models.HiraganaLetter.objects.all()
 
 
-class KatakanaListView(TemplateView):
+class KatakanaListView(ListView):
     """Класс представления списка букв катаканы."""
-    template_name = 'writing_system/base.html'
+    template_name = 'writing_system/list_katakana.html'
+    context_object_name = 'katakana_letters'
+
+    def get_queryset(self) -> QuerySet[models.KatakanaLetter]:
+        return models.KatakanaLetter.objects.all()
 
 
-class KanjiListView(TemplateView):
+class KanjiListView(ListView):
     """Класс представления списка букв кандзи."""
-    template_name = 'writing_system/base.html'
+    template_name = 'writing_system/list_kanji.html'
+    context_object_name = 'kanji_letters'
+    paginate_by = 50
+
+    def get_queryset(self) -> QuerySet[models.KanjiLetter]:
+        return models.KanjiLetter.objects.all()
 
 
 class HiraganaFormView(TemplateView):
