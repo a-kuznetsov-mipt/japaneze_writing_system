a8d6b77eeb98caea74f89452251fe11dc5a5fcab
Alexander Kuznetsov <166259762+a-kuznetsov-mipt@users.noreply.github.com>
2024-05-20 21:29:24 +0300

    Refactoring: names of routes to creation forms changed

--- writing_system/urls.py
+++ writing_system/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('list_hiragana', views.HiraganaListView.as_view(), name='list-hiragana'),
     path('list_katakana', views.KatakanaListView.as_view(), name='list-katakana'),
     path('list_kanji', views.KanjiListView.as_view(), name='list-kanji'),
-    path('create_hiragana', views.HiraganaFormView.as_view(), name='form-hiragana'),
-    path('create_katakana', views.KatakanaFormView.as_view(), name='form-katakana'),
-    path('create_kanji', views.KanjiFormView.as_view(), name='form-kanji'),
+    path('create_hiragana', views.HiraganaFormView.as_view(), name='create-hiragana'),
+    path('create_katakana', views.KatakanaFormView.as_view(), name='create-katakana'),
+    path('create_kanji', views.KanjiFormView.as_view(), name='create-kanji'),
     path('creation_froms_list', views.CreationFromsList.as_view(), name='creation-forms-list'),
 ]
